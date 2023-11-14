58b1401e1668cdfd358dc1808fbbf747085bb1a5
Savindu Nimsara <149399456+MrNimaOfc@users.noreply.github.com>
2023-11-14 18:07:20 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -14,7 +14,7 @@ class mr(object):
     ABOUT_TXT = """
 ╭───────────⍟
 ├🤖 My Name is : {}
-├👑 CHANNEL : <a href=https://t.me/MrNimaUi>Nima's Blog</a> 
+├👑 CHANNEL : <a href=https://t.me/MrNima_Ofc>Nima's Blog</a> 
 ├📕 LIBRARY : <a href=https://github.com/pyrogram>Pyrogram</a>
 ├✏️ LANGUAGE : <a href=https://www.python.org>Python 3</a>
 ├💾 DATABASE : <a href=https://cloud.mongodb.com>MongoDB</a>
